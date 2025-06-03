7381c1b8e074ffdef9dc9d328334372b1cf2d1a6
Valasai <valasaict582@gmail.com>
2025-06-03 12:03:45 +0500

    Refactor: convert to functional react component

--- traffic-light/src/TrafficLightSimulator.jsx
+++ traffic-light/src/TrafficLightSimulator.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
+import React, { use, useEffect, useState } from 'react'
 
-// This component is a placeholder for the Traffic Light Simulator
 function TrafficLightSimulator() {
-  return (
+    
+    return (
     <>
-        <div style={{color: 'white', fontFamily: 'sans-serif'}} className='traffic-light' >
+        <div style={{color: 'white', fontFamily: 'sans-serif'}} className='traffic-light'>
             <p>Traffic Light Simulator</p>
             <div style={{padding: 40, backgroundColor: '#3e4952', width: 50, height: 150, boxShadow: '0 0 10px black', borderRadius: 10, display: 'flex', flexDirection: 'column', justifyContent: 'space-evenly'}}>
-                <div style={{backgroundColor: 'gray', width: 50, height: 50, borderRadius: '50%', }}></div>
-                <div style={{backgroundColor: 'gray', width: 50, height: 50, borderRadius: '50%', }}></div>
-                <div style={{backgroundColor: 'gray', width: 50, height: 50, borderRadius: '50%', }}></div>
+                <div style={{backgroundColor: 'gray', width: 50, height: 50, borderRadius: '50%'}}></div>
+                <div style={{backgroundColor: 'gray', width: 50, height: 50, borderRadius: '50%'}}></div>
+                <div style={{backgroundColor: 'gray', width: 50, height: 50, borderRadius: '50%'}}></div>
             </div>
         </div>
     </>
